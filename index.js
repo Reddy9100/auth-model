48d750c7f26229e2cf0adbe815106e6219ee2d48
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-04-24 12:28:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 const port = 3000;
 
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise"); // Importing the promise-based version
 const routesOfUser = require("./routes/crudRotes");
 
 // Create MySQL connection
@@ -20,25 +20,11 @@ const connection = mysql.createConnection({
 
 // Function to start the server
 function startServer(connection) {
-    connection.connect(function(err) {
-        if (err) {
-            console.error("ERR WHILE CONNECTING TO DB", err);
-            return;
-        }
-        console.log("DATABASE_CONNECTION_ESTABLISHED");
+    console.log("DATABASE_CONNECTION_ESTABLISHED");
 
-        const query = "SELECT * FROM users";
-        connection.query(query, function(err, result) {
-            if (err) {
-                console.error("ERROR EXECUTING QUERY", err);
-                return;
-            }
-            console.log(result);
-            // Start the server once the database connection is established
-            app.listen(port, function() {
-                console.log(`SERVER CONNECTION IS ESTABLISHED WITH PORT ${port}`);
-            });
-        });
+    // Start the server once the database connection is established
+    app.listen(port, function() {
+        console.log(`SERVER CONNECTION IS ESTABLISHED WITH PORT ${port}`);
     });
 }
 
@@ -53,4 +39,4 @@ app.get("/", (req, res) => {
 // Mount the CRUD routes
 app.use("/api", routesOfUser);
 
-module.exports  = connection
+module.exports = connection;
